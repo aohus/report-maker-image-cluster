3775447340fe3718a6a19cf3a4675f9729a09fe0
aohus <dbtnghk528@gmail.com>
2025-12-02 12:30:18 +0900

    modify: 책임 분리

---
+++ core/deps.py
@@ -0,0 +1,9 @@
+from fastapi import Depends, Request
+
+
+def get_pipeline(request: Request):
+    return request.app.state.pipeline
+
+
+def get_lock(request: Request):
+    return request.app.state.cluster_lock
